764263ac4eb2b104ed6891feb8e10292edd45be7
Alex Fomin <shurickfomin@gmail.com>
2020-06-24 14:39:03 +0300

    Use AutoMocker

--- SuperDuperService.Tests/SmartWeatherServiceTests.cs
+++ SuperDuperService.Tests/SmartWeatherServiceTests.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
+using Moq.AutoMock;
 using SuperDuperService.Models;
 using SuperDuperService.Services;
 using Xunit;
@@ -15,13 +16,13 @@ namespace SuperDuperService.Tests
 		[Fact]
 		public async Task GetTemperatureAsync_ShouldUseFirstLocation()
 		{
-			var locationService = new Mock<ILocationService>();
-			locationService.Setup(x => x.GeocodeAsync("Minsk"))
+			var mocker = new AutoMocker();
+
+			mocker.GetMock<ILocationService>()
+				.Setup(x => x.GeocodeAsync("Minsk"))
 				.ReturnsAsync(new[] { new GeoLocation { Lat = 1, Lon = 1 }, new GeoLocation { Lat = 2, Lon = 2 } });
 
-			var weatherService = new Mock<IWeatherService>();
-
-			weatherService.Setup(x => x.GetWeatherAsync(It.Is<GeoLocation>(l => l.Lat == 1 && l.Lon == 1)))
+			mocker.GetMock<IWeatherService>().Setup(x => x.GetWeatherAsync(It.Is<GeoLocation>(l => l.Lat == 1 && l.Lon == 1)))
 				.ReturnsAsync(new Weather
 				{
 					Condition = Condition.Sunny,
@@ -29,7 +30,7 @@ namespace SuperDuperService.Tests
 					Description = "Hot"
 				});
 
-			var service = new SmartWeatherService(Mock.Of<ILogger<SmartWeatherService>>(), weatherService.Object, locationService.Object, Mock.Of<IMemoryCache>());
+			var service = mocker.CreateInstance<SmartWeatherService>();
 
 			var actualWeather = await service.GetRealTimeWeatherAsync("Minsk");
 
