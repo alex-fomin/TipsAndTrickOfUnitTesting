a6f0e35991e48e05a5de45f47ece63f013844472
Alex Fomin <shurickfomin@gmail.com>
2020-06-24 14:55:53 +0300

    Fix Unit Tests

--- SuperDuperService.Tests/LocationIQServiceTests.cs
+++ SuperDuperService.Tests/LocationIQServiceTests.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using Microsoft.Extensions.Options;
 using Moq;
 using Moq.AutoMock;
+using RichardSzalay.MockHttp;
 using SuperDuperService.Models;
 using SuperDuperService.Services.LocationIQ;
 using Xunit;
@@ -17,6 +18,13 @@ namespace SuperDuperService.Tests
 			var mocker = new AutoMocker(MockBehavior.Strict);
 
 			mocker.Use(Options.Create(new LocationIQServiceSettings { ApiKey = "apiKey" }));
+
+			var mockHttp = new MockHttpMessageHandler();
+
+			mockHttp.When("https://eu1.locationiq.com//v1/reverse.php?key=apiKey&lat=54&lon=27&format=json")
+				.Respond("application/json", @"{ ""address"": {  ""region"": ""Minsk Region""  } }");
+
+			mocker.Use(mockHttp.ToHttpClient());
 
 			var service = mocker.CreateInstance<LocationIQService>();
 
